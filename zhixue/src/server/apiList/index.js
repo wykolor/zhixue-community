7e97a07179870b219f5d675eaf37a63e01de1cc9
kcolor <1121460147@qq.com>
2019-12-19 21:48:12 +0800

    完善项目搭建

--- zhixue/src/server/apiList/index.js
+++ zhixue/src/server/apiList/index.js
@@ -1,9 +1,12 @@
-import { get, post } from "../axios";
+import {
+  get,
+  post
+} from "../axios";
 
 //
 let login = "http://mpestate.dev.smartyface.cn/api/v1/wechat/updateUserInfo";
 export default {
-  loginAddress: function(loginObj) {
-    return get(login, loginObj);
+  loginAddress: function (loginObj) {
+    return post(login, loginObj);
   }
 };
