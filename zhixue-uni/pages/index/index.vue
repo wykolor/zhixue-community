6d7334ba22cfc74c49dbf0fcd6ae10f7341ed8cc
kcolor <1121460147@qq.com>
2019-12-21 00:13:04 +0800

    轮播图功能

--- zhixue-uni/pages/index/index.vue
+++ zhixue-uni/pages/index/index.vue
@@ -1,6 +1,21 @@
 <template>
-	<view>
-		我是首页
+	<view class="home">
+		<view class="home-banner">
+			<swiper class="swiper" :indicator-dots="true" :autoplay="true" :interval="2000" :duration="500" indicator-active-color="pink" indicator-color="#fff">
+				<swiper-item v-for="item in banerImage" :key="item.id">
+					<image :src="item.image"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+		<view class="home-server">
+			<view>
+				<image :src="homeServer[0].image"></image>
+			</view>
+			<view>
+				<image :src="homeServer[1].image" ></image>
+				<image :src="homeServer[2].image" ></image>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,7 +23,28 @@
 	export default {
 		data() {
 			return {
-				
+				banerImage:[
+					{
+						"image": require("../../static/img/index/u118.jpg")
+					},
+					{
+						"image": require("../../static/img/index/u118.jpg")
+					},
+					{
+						"image": require("../../static/img/index/u118.jpg")
+					}
+				],
+				homeServer:[
+					{
+						"image": require("../../static/img/index/u129.png")
+					},
+					{
+						"image": require("../../static/img/index/u130.png")
+					},
+					{
+						"image": require("../../static/img/index/u131.png")
+					},
+				]
 			};
 		}
 	}
@@ -16,4 +52,9 @@
 
 <style lang="scss">
 
+.home-server{
+	width: 100%;
+	box-sizing: border-box;
+	display: flex;
+}
 </style>
