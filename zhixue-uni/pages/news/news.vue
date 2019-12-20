131ef87ad5d47a3693591b397663b29b651905cf
kcolor <1121460147@qq.com>
2019-12-20 22:43:46 +0800

    首页导航栏及底部tab实现完毕

---
+++ zhixue-uni/pages/news/news.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		我是消息界面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ zhixue-uni/pages/server/server.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		我是服务界面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
