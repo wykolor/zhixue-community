8099ac4814b8abdfa3febcd94f638cdff8193a54
wykolor <kolor1121460147@163.com>
2019-12-23 19:37:55 +0800

    我的静态界面实现完毕

--- zhixue-uni/pages/mine/mine.vue
+++ zhixue-uni/pages/mine/mine.vue
@@ -1,6 +1,17 @@
 <template>
-	<view>
-		我是我的界面
+	<view class="mine">
+		<view class="mine-header">
+			<van-cell title="登录/注册"  size="large" isLink center :border="false">
+				<van-icon slot="icon" name="smile" size="4rem" custom-style="margin-right:10px"></van-icon>
+			</van-cell>
+		</view>
+		<van-cell-group :border="false">
+			<van-cell title="人脸管理"  isLink icon="coupon-o">
+			</van-cell>
+			<van-cell title="物业电话"  isLink icon="phone"/>
+			<van-cell title="智能门禁"  isLink icon="smile-o"/>
+			<van-cell title="意见反馈" isLink icon="ellipsis"/>
+		</van-cell-group>
 	</view>
 </template>
 
@@ -14,6 +25,23 @@
 	}
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.mine{
+	width: 100%;
+	min-height: 100vh;
+	background: #fff;
+	@include border-box;
+	.mine-header{
+		& /deep/ .van-cell{
+			padding-bottom: 3rem;
+			padding-top: 2rem;
+			background-color: $zx-topic-color;
+			color: #fff;
+			.van-cell__right-icon-wrap{
+				color: #fff;
+			}
+		}
+	}
+		
+}
 </style>
