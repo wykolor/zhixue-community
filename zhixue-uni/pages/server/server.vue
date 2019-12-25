a8892291328ba5d14775685a7619ea8ee4a36971
wykolor <kolor1121460147@163.com>
2019-12-25 20:43:32 +0800

    服务静态界面完毕

--- zhixue-uni/pages/server/server.vue
+++ zhixue-uni/pages/server/server.vue
@@ -1,6 +1,10 @@
 <template>
-	<view>
-		我是服务界面
+	<view class="server">
+		<van-cell-group :border="false" class="server-list" v-for="item in serverList" :key="item.id" :title="item.categrayName">
+			<van-grid column-num="3" square clickable >
+			  <van-grid-item icon="photo-o" :text="list.title" v-for="list in item.list" :key="list.id"/>
+			</van-grid>
+		</van-cell-group>
 	</view>
 </template>
 
@@ -8,12 +12,57 @@
 	export default {
 		data() {
 			return {
-				
+				serverList:[
+					{
+						categrayName:"物业服务",
+						list:[
+							{
+								title:"小区公告"
+							},
+							{
+								title:"小区报修"
+							},
+							{
+								title:"意见建议"
+							},
+							{
+								title:"物业缴费"
+							},
+							{
+								title:"手机开门"
+							},
+							{
+								title:"便民号码"
+							}
+						]
+					},
+					{
+						categrayName:"小区互动",
+						list:[
+							{
+								title:"小区活动"
+							},
+							{
+								title:"小区家政"
+							},
+							{
+								title:"房屋租售"
+							},
+							{
+								title:"小区拼车"
+							},
+						]
+					}
+				]
 			};
 		}
 	}
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.server{
+	@include init-style;
+	background-color: $uni-bg-color-page;
+	padding-bottom: 1rem;
+}
 </style>
